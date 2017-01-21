6a9d4bd6c7145c8c732041731ffd01b50dfc6a83
Clement Linel <clement.linel@gmail.com>
2017-01-21 10:33:45 -0500

    Rotate reactor to be parallel to ground Added character import script.

---
+++ GlobalWaveJam/Assets/Scripts/OrientReactor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrientReactor : MonoBehaviour
+{
+    [SerializeField]
+    private float _raycastLength = 3f;
+
+    [SerializeField]
+    private LayerMask _layersToConsider;
+
+    void LateUpdate()
+    {
+        RaycastHit result;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out result, _raycastLength, _layersToConsider))
+        {
+            Quaternion rotateToNormal = Quaternion.FromToRotation(transform.up, result.normal);
+            transform.rotation = rotateToNormal * transform.rotation;
+        }
+    }
+}
