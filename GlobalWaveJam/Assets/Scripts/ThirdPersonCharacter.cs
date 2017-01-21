96d214636c96e8f859bdb0a8214a85dfa9511830
Clement Linel <clement.linel@gmail.com>
2017-01-21 13:05:01 -0500

    Added aiborne controls and safety to prevent the waves from moving above the character. TODO: This could enable players that are out of the arena to jump back on it by going below.

--- GlobalWaveJam/Assets/Scripts/ThirdPersonCharacter.cs
+++ GlobalWaveJam/Assets/Scripts/ThirdPersonCharacter.cs
@@ -5,13 +5,6 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof(SphereCollider))]
 public class ThirdPersonCharacter : MonoBehaviour
 {
-    [SerializeField] float m_MovingTurnSpeed = 360;
-    [SerializeField] float m_StationaryTurnSpeed = 180;
-    [SerializeField] float m_StompPower = 12f;
-    [Range(1f, 4f)] [SerializeField] float m_GravityMultiplier = 2f;
-    [SerializeField] float m_MoveSpeedMultiplier = 1f;
-    [SerializeField] float m_GroundCheckDistance = 0.1f;
-
     public Animator m_Animator;
 
     Rigidbody m_Rigidbody;
@@ -22,11 +15,35 @@ public class ThirdPersonCharacter : MonoBehaviour
     Vector3 m_GroundNormal;
     SphereCollider m_SphereCollider;
 
+    [SerializeField]
+    float m_StompPower = 12f;
+    [SerializeField]
+    private LayerMask _layersToConsiderForGround;
+    [SerializeField]
+    float m_BelowGroundCheckDistance = 10f;
+
+    [HeaderAttribute("Grounded Control")]
+    [SerializeField]
+    float m_MovingTurnSpeedGrounded = 360;
+    [SerializeField]
+    float m_StationaryTurnSpeedGrounded = 180;
+    [SerializeField]
+    float m_MoveSpeedMultiplierGrounded = 1f;
+    [SerializeField]
+    float m_GroundCheckDistance = 0.1f;
+
+    [HeaderAttribute("Airborne Control")]
+    [SerializeField]
+    float m_MovingTurnSpeedAirborne = 360;
+    [SerializeField]
+    float m_StationaryTurnSpeedAirborne = 180;
+    [SerializeField]
+    float m_MoveSpeedMultiplierAirborne = 1f;
+    [Range(1f, 4f)]
+    [SerializeField]
+    float m_GravityMultiplier = 2f;
+
     [HeaderAttribute("Jump")]
-    [SerializeField]
-    private float _raycastLengthForGroundNormal = 3f;
-    [SerializeField]
-    private LayerMask _layersToConsiderForGroundNormal;
     [SerializeField]
     float m_JumpPower = 12f;
 
@@ -38,7 +55,6 @@ public class ThirdPersonCharacter : MonoBehaviour
         m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
         m_OrigGroundCheckDistance = m_GroundCheckDistance;
     }
-
 
     public void Move(Vector3 move, bool crouch, bool jump)
     {
@@ -55,16 +71,45 @@ public class ThirdPersonCharacter : MonoBehaviour
         m_TurnAmount = Mathf.Atan2(move.x, move.z);
         m_ForwardAmount = move.z;
 
-        ApplyExtraTurnRotation();
+        float moveSpeedMultiplier = 1f;
 
         // control and velocity handling is different when grounded and airborne:
         if (m_IsGrounded)
         {
-            HandleGroundedMovement(crouch, jump);
+            // help the character turn faster (this is in addition to root rotation in the animation)
+            float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeedGrounded, m_MovingTurnSpeedGrounded, m_ForwardAmount);
+            transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
+
+            // check whether conditions are right to allow a jump:
+            if (jump && !crouch && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
+            {
+                // jump!
+                m_Rigidbody.AddForce(m_GroundNormal * m_JumpPower, ForceMode.Impulse);
+                m_IsGrounded = false;
+                m_GroundCheckDistance = 0.1f;
+            }
+
+            moveSpeedMultiplier = m_MoveSpeedMultiplierGrounded;
         }
         else
         {
-            HandleAirborneMovement(crouch, jump);
+            // help the character turn faster (this is in addition to root rotation in the animation)
+            float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeedAirborne, m_MovingTurnSpeedAirborne, m_ForwardAmount);
+            transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
+
+            // apply extra gravity from multiplier:
+            Vector3 extraGravityForce = (Physics.gravity * m_GravityMultiplier) - Physics.gravity;
+            m_Rigidbody.AddForce(extraGravityForce);
+
+            m_GroundCheckDistance = m_Rigidbody.velocity.y < 0 ? m_OrigGroundCheckDistance : 0.01f;
+
+            if (jump && !crouch && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Airborne"))
+            {
+                // stomp!
+                m_Rigidbody.AddRelativeForce(-Vector3.up * m_StompPower, ForceMode.Impulse);
+            }
+
+            moveSpeedMultiplier = m_MoveSpeedMultiplierAirborne;
         }
 
         // send input and other state parameters to the animator
@@ -72,10 +117,10 @@ public class ThirdPersonCharacter : MonoBehaviour
 
         // we implement this function to override the default root motion.
         // this allows us to modify the positional speed before it's applied.
-        if (/*m_IsGrounded && */Time.deltaTime > 0)
+        if (Time.deltaTime > 0)
         {
             m_Rigidbody.AddRelativeTorque(Vector3.up * m_TurnAmount, ForceMode.VelocityChange);
-            m_Rigidbody.AddRelativeForce(Vector3.forward * m_ForwardAmount * m_MoveSpeedMultiplier, ForceMode.VelocityChange);
+            m_Rigidbody.AddRelativeForce(Vector3.forward * m_ForwardAmount * moveSpeedMultiplier, ForceMode.VelocityChange);
         }
     }
 
@@ -91,50 +136,6 @@ public class ThirdPersonCharacter : MonoBehaviour
         }
     }
 
-
-    void HandleAirborneMovement(bool crouch, bool jump)
-    {
-        // apply extra gravity from multiplier:
-        Vector3 extraGravityForce = (Physics.gravity * m_GravityMultiplier) - Physics.gravity;
-        m_Rigidbody.AddForce(extraGravityForce);
-
-        m_GroundCheckDistance = m_Rigidbody.velocity.y < 0 ? m_OrigGroundCheckDistance : 0.01f;
-
-        if (jump && !crouch && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Airborne"))
-        {
-            // stomp!
-            m_Rigidbody.AddRelativeForce(-Vector3.up * m_StompPower, ForceMode.Impulse);
-        }
-    }
-
-
-    void HandleGroundedMovement(bool crouch, bool jump)
-    {
-        // check whether conditions are right to allow a jump:
-        if (jump && !crouch && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
-        {
-            Vector3 jumpDirection = Vector3.up;
-            RaycastHit result;
-
-            if (Physics.Raycast(transform.position, Vector3.down, out result, _raycastLengthForGroundNormal, _layersToConsiderForGroundNormal))
-            {
-                jumpDirection = result.normal;
-            }
-
-            // jump!
-            m_Rigidbody.AddForce(jumpDirection * m_JumpPower, ForceMode.Impulse);
-            m_IsGrounded = false;
-            m_GroundCheckDistance = 0.1f;
-        }
-    }
-
-    void ApplyExtraTurnRotation()
-    {
-        // help the character turn faster (this is in addition to root rotation in the animation)
-        float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
-        transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
-    }
-
     void CheckGroundStatus()
     {
         RaycastHit hitInfo;
@@ -142,9 +143,9 @@ public class ThirdPersonCharacter : MonoBehaviour
         // helper to visualise the ground check ray in the scene view
         Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
 #endif
-        // 0.1f is a small offset to start the ray from inside the character
+        // 0.1f is a small offset to start the ray from inside the character 
         // it is also good to note that the transform position in the sample assets is at the base of the character
-        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance))
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance, _layersToConsiderForGround))
         {
             m_GroundNormal = hitInfo.normal;
             m_IsGrounded = true;
@@ -153,6 +154,16 @@ public class ThirdPersonCharacter : MonoBehaviour
         {
             m_IsGrounded = false;
             m_GroundNormal = Vector3.up;
+
+#if UNITY_EDITOR
+            // helper to visualise the ground check ray in the scene view
+            Debug.DrawLine(transform.position + m_BelowGroundCheckDistance * Vector3.up, transform.position, Color.cyan);
+#endif
+            // Not grounded. Check if the ground is above us
+            if (Physics.Raycast(transform.position + m_BelowGroundCheckDistance * Vector3.up, Vector3.down, out hitInfo, m_BelowGroundCheckDistance, _layersToConsiderForGround))
+            {
+                transform.position = hitInfo.point;
+            }
         }
     }
 }
