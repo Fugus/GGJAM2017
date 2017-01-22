84bc14dba9b95ef6e3b3d0635936a63acb60cafc
ULTRA-MMOI\Fab <fabula.leonis@gmail.com>
2017-01-22 13:13:41 +0900

    - new UI camera, ui result

--- GlobalWaveJam/Assets/Scripts/Game/UIResults.cs
+++ GlobalWaveJam/Assets/Scripts/Game/UIResults.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UIResults : MonoBehaviour {
+public class UIResults : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        foreach (PlayerStats p in GameLogic.Players)
+        {
+            string dbgtxt = "Chara " + p.Index;
+            int newscore = p.Score;
+            foreach (KeyValuePair<TrackingEvent, int> pair in p.Events)
+            {
+                newscore += GameSettings.ScoreMatrix[pair.Key] * pair.Value;
+            }
+
+            foreach (Text t in FindObjectsOfType<Text>())
+            {
+                if (t.name == "Results_" + p.Index)
+                {
+                    t.text = "" + newscore;
+                    Debug.Log(t.name + " " + t.text);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
