438350d69bf5a8e186c8c3bb39d8740de2a30f65
ULTRA-MMOI\Fab <fabula.leonis@gmail.com>
2017-01-22 14:35:50 +0900

    - added camera shake, ui colors

---
+++ GlobalWaveJam/Assets/Scripts/Game/UIColorer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Script to add to any UI parent component you'd like to follow the characters color codes
+/// </summary>
+public class UIColorer : MonoBehaviour {
+
+    // settings
+    public Material Emission;
+
+	// Use this for initialization
+	void Start () {
+        if (Emission == null)
+            return;
+
+        Color c = Emission.GetColor("_EmissionColor");
+
+        foreach (Text t in GetComponentsInChildren<Text>())
+        {
+            t.color = c;
+        }
+
+        foreach (RawImage i in GetComponentsInChildren<RawImage>())
+        {
+            i.color = c;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
